a43d36cfad99278b0baa09422bdbbce7ac120e5b
vtymkiv <vtymkiv@yahoo.com>
2025-10-11 11:23:12 +0300

    Add the ability to read all rows from document and convert group of rows to columns

--- w2e-core/src/main/java/com/w2e/core/service/docx/DocxServiceConvColToRowsImpl.java
+++ w2e-core/src/main/java/com/w2e/core/service/docx/DocxServiceConvColToRowsImpl.java
@@ -5,6 +5,7 @@ import com.w2e.core.model.DocTableCell;
 import com.w2e.core.model.DocTableRow;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
 import org.apache.poi.xwpf.usermodel.XWPFTableCell;
@@ -16,22 +17,24 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Builder
 public class DocxServiceConvColToRowsImpl implements DocxService {
+    private int numOfRowsToColumns;
 
     @Override
     public List<DocTableRow> readDocument(String pathToDocument) {
         List<DocTableRow> docTableRowList = new ArrayList<>();
+        numOfRowsToColumns = 7;
         try (InputStream is = Files.newInputStream(Paths.get(pathToDocument));
              XWPFDocument doc = new XWPFDocument(is)) {
             List<XWPFTable> tables = doc.getTables();
 
             if (tables.isEmpty()) {
-                log.error("No tables found in the document.");
-                throw new RuntimeException("No tables found in the document.");
+                String errMsg = "No tables found in the document.";
+                log.error(errMsg);
+                throw new RuntimeException(errMsg);
             }
 
             for (int i = 0; i < tables.size(); i++) {
@@ -39,35 +42,51 @@ public class DocxServiceConvColToRowsImpl implements DocxService {
                 log.debug("\n--- Table N%{}---", i + 1);
 
                 // Read sequentially number of rows that would be converted into
-                // excel columns
+                // Excel columns
                 int numberOfRows = table.getNumberOfRows();
-                XWPFTableRow firstRow = Optional.ofNullable(table.getRow(0)).orElseThrow(() -> new RuntimeException("Table does contain rows"));
-                int numberOfCells = firstRow.getTableCells().size();
-                int shift_rows = 0;
+                //XWPFTableRow firstRow = Optional.ofNullable(table.getRow(0)).orElseThrow(() -> new RuntimeException("Table does contain rows"));
+
                 int cell_to_read = 1;
                 // Covert columns to rows
 
                 // Number of columns will become number of row and number of rows will become
                 // number off columns. Number of row is limited to 7
-                int newRow = 0;
-                int newCell = 1;
 
-                List<DocTableCell> docTableCellList = new ArrayList<>(numberOfRows);
+                List<DocTableCell> docTableCellList = new ArrayList<>();
+                int newCell = 1;
+                int newRow = 0;
+                int rowIdx = 0;
+                // Loop via rows in document
                 for (int rowNum = 0; rowNum < numberOfRows; rowNum++) {
                     XWPFTableRow row = table.getRow(rowNum);
+                    if (isEmptyRow(row)) {
+                        continue;
+                    }
+                    rowIdx++;
+                    if (rowIdx % numOfRowsToColumns == 0) {
+                        docTableRowList.add(DocTableRow.builder()
+                                .rowNum(newRow++)
+                                .cellList(docTableCellList)
+                                .build());
+                        docTableCellList = new ArrayList<>();
+                        newCell = 1;
 
-                    XWPFTableCell cell = row.getCell(cell_to_read);
-                    docTableCellList.add(DocTableCell.builder()
-                            .cellPos(newCell++)
-                            .text(cell.getText())
+                    } else {
+                        XWPFTableCell cell = row.getCell(cell_to_read);
+                        docTableCellList.add(DocTableCell.builder()
+                                .cellPos(newCell++)
+                                .text(cell.getText())
+                                .build());
+                    }
+
+                }
+                // In case if have odd number of rows
+                if (CollectionUtils.isNotEmpty(docTableCellList)) {
+                    docTableRowList.add(DocTableRow.builder()
+                            .rowNum(newRow++)
+                            .cellList(docTableCellList)
                             .build());
                 }
-                docTableRowList.add(DocTableRow.builder()
-                        .rowNum(newRow++)
-                        .cellList(docTableCellList)
-                        .build());
-
-
                 log.info("Rows in table: {}", docTableRowList.size());
 
             }
@@ -77,5 +96,17 @@ public class DocxServiceConvColToRowsImpl implements DocxService {
             throw new RuntimeException(e);
         }
         return docTableRowList;
+    }
+
+    private boolean isEmptyRow(XWPFTableRow row) {
+        if (row == null) {
+            return true;
+        }
+        for (XWPFTableCell cell : row.getTableCells()) {
+            if (cell != null && !cell.toString().trim().isEmpty()) {
+                return false;
+            }
+        }
+        return false;
     }
 }
--- w2e-core/src/main/java/module-info.java
+++ w2e-core/src/main/java/module-info.java
@@ -2,5 +2,6 @@ module w2e.core {
     requires static lombok;
     requires org.apache.poi.ooxml;
     requires org.slf4j;
+    requires org.apache.commons.collections4;
     exports com.w2e.core;
 }
